b7bc6aaf641860ee513f99b9ce81752f3d1c30f0
Waldemar Tomme <waldemar.tomme@bluehands.de>
2019-02-12 18:15:07 +0100

    Add project files.

---
+++ JenkinsSlaveSetupShellWrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsSlaveSetupShellWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
